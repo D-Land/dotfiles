33709cb9d2b88ebac067df5ddea27d411dfc293d
Drew Land <drewmland@gmail.com>
2014-02-18 14:30:07 -0500

    Update prompt and comments

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -15,22 +15,22 @@ HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
 
-setopt NO_BG_NICE # don't nice background tasks
+setopt NO_BG_NICE       # don't nice background tasks
 setopt NO_HUP
 setopt NO_LIST_BEEP
-setopt LOCAL_OPTIONS # allow functions to have local options
-setopt LOCAL_TRAPS # allow functions to have local traps
+setopt LOCAL_OPTIONS    # allow functions to have local options
+setopt LOCAL_TRAPS      # allow functions to have local traps
 setopt HIST_VERIFY
-setopt SHARE_HISTORY # share history between sessions ???
+setopt SHARE_HISTORY    # share history between sessions ???
 setopt EXTENDED_HISTORY # add timestamps to history
 setopt PROMPT_SUBST
 setopt CORRECT
 setopt COMPLETE_IN_WORD
 setopt IGNORE_EOF
 
-setopt APPEND_HISTORY # adds history
+setopt APPEND_HISTORY                    # adds history
 setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
-setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
+setopt HIST_IGNORE_ALL_DUPS              # don't record dupes in history
 setopt HIST_REDUCE_BLANKS
 
 # don't expand aliases _before_ completion has finished
@@ -47,5 +47,6 @@ bindkey '^[[3~' delete-char
 bindkey '^[^N' newtab
 bindkey '^?' backward-delete-char
 
+#Allows backward history completion
 bindkey "^[[A" history-search-backward
 bindkey "^[[B" history-search-forward
--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -51,7 +51,7 @@ directory_name() {
 }
 
 the_time() {
-  echo "%{$fg_bold[magenta]%}%D{%a %b %e %Y %l:%M%p}%{$reset_color%}"
+  echo "%{$fg_bold[magenta]%}%D{%a %b %e %Y%l:%M%p}%{$reset_color%}"
 }
 
 export PROMPT=$'\n$(the_time) $(directory_name) $(git_dirty)$(need_push)\n› '
