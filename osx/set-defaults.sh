aac07c6d6aebddb935257c5700b470f2d60e4915
Drew Land <drewmland@gmail.com>
2014-04-08 14:51:56 -0400

    Set some more OSX defaults

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -4,11 +4,10 @@
 #
 # The original idea (and a couple settings) were grabbed from:
 #   https://github.com/mathiasbynens/dotfiles/blob/master/.osx
+# Additional resouces
+#   https://github.com/ymendel/dotfiles/tree/master/osx
 #
 # Run ./set-defaults.sh and you'll be good to go.
-
-# Disable press-and-hold for keys in favor of key repeat.
-defaults write -g ApplePressAndHoldEnabled -bool false
 
 # Use AirDrop over every interface. srsly this should be a default.
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
@@ -19,15 +18,36 @@ defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 # Show the ~/Library folder.
 chflags nohidden ~/Library
 
+## Input Devices
+# Disable press-and-hold for keys in favor of key repeat.
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+# Enable tap-to-click
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -bool true
+
+# Disable "natural" (touchscreen-style) scrolling
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+
 # Set a really fast key repeat.
 defaults write NSGlobalDomain KeyRepeat -int 0
 
-# Set the Finder prefs for showing a few different volumes on the Desktop.
-# defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
-# defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+## Dock
+# Place dock of left side of screen
+defaults write com.apple.dock orientation -string left
 
-# Run the screensaver if we're in the bottom-left hot corner.
-defaults write com.apple.dock wvous-tl-corner -int 10
+# Dock auto hide/show
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock autohide-delay -float 0.0
+
+# Dock icon size
+defaults write com.apple.dock tilesize -integer 18
+
+## Mission Control
+# Don't show the dashboard as a space
+defaults write com.apple.dock dashboard-in-overlay -bool true
+
+# Put screen to sleep if we're in the top left corner
+defaults write com.apple.dock wvous-tl-corner -int 3
 defaults write com.apple.dock wvous-tl-modifier -int 0
 
 # Set up Safari for development.
