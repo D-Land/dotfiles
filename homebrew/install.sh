0d069b0be12de7c7cf1c352c2b60b599d0895b4d
Drew Land <drewmland@gmail.com>
2014-04-23 19:45:42 -0400

    Fix file path issues with formulas and casks files

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -25,16 +25,17 @@ fi
 all_formulas=''
 while read formula; do
   all_formulas=$all_formulas$(echo $formula | tr '\n' ' ')
-done < formulas
-
+done < $(dirname "${BASH_SOURCE[0]}")/formulas
 brew install $all_formulas
 
 # Install homebrew casks
 all_casks=''
 while read cask; do
-  all_casks=$all_casks$(echo $cask | tr '\n' ' ')
-done < casks
-
+  if test ! $(brew cask list | grep $cask)
+  then
+    all_casks=$all_casks$(echo $cask | tr '\n' ' ')
+  fi
+done < $(dirname "${BASH_SOURCE[0]}")/casks
 brew cask install $all_casks
 
 exit 0
