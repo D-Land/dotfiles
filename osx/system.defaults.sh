41f04c62856ba29bdb73e1069a56625597121528
Drew Land <drewmland@gmail.com>
2014-04-24 13:19:24 -0400

    Restore power button functionality to old way

---
+++ osx/system.defaults.sh
@@ -0,0 +1,4 @@
+# Sets reasonable OS X defaults for the system
+
+# Instead of putting Mac to sleep it brings up options menu for powering on, off etc...
+defaults write com.apple.loginwindow PowerButtonSleepsSystem -bool no
