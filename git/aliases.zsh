d3d68c7d17a7459ac5fcb40e08fac2da7765b54d
Drew Land <drewmland+github@gmail.com>
2015-05-20 14:35:35 -0400

    Add aliases for git to show dl/ branches list and another for showing what files a commit touches

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -19,3 +19,5 @@ alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gg='gs'
 alias gcm='git commit -m'
+alias gmb='git branch -a | grep dl/'
+alias gsf='git show --pretty="format:" --name-only'
