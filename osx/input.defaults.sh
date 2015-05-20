39c397ab3693a8c6d7cb639721ca5a37e3d10b3d
Drew Land <drewmland+github@gmail.com>
2015-05-20 14:32:18 -0400

    Enable tap to click and 3 finger drag

--- osx/input.defaults.sh
+++ osx/input.defaults.sh
@@ -9,7 +9,11 @@ defaults write NSGlobalDomain KeyRepeat -int 0
 
 ## Mouse
 # Enable tap-to-click
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
 defaults write NSGlobalDomain com.apple.mouse.tapBehavior -bool true
+
+# Enable 3 finger drag
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag -bool true
 
 # Disable "natural" (touchscreen-style) scrolling
 defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
