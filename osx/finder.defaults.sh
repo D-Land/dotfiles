65daed84fa2b45149268ebe100361cd77697d5ef
Drew Land <drewmland@gmail.com>
2015-05-22 13:41:03 -0400

    Finder default to list view and show all file extentions

--- osx/finder.defaults.sh
+++ osx/finder.defaults.sh
@@ -4,10 +4,17 @@
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
 
 # Always open everything in Finder's list view. This is important.
-defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+
+# Show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
 # Show the ~/Library folder.
 chflags nohidden ~/Library
+
+# Expand save panel by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
 
 ## Mission Control
 # Don't show the dashboard as a space
