95226961cacb93e0390ac8cf666cc0e8372a4171
Drew Land <drewmland@gmail.com>
2015-05-24 11:55:53 -0400

    Create menu defaults file and add system menu icon ordering default

--- osx/dock.defaults.sh
+++ osx/dock.defaults.sh
@@ -33,7 +33,4 @@ defaults write com.apple.dock wvous-tl-modifier -int 0
 # Speed up expose' animation
 defaults write com.apple.dock expose-animation-duration -float 0.2
 
-# Show battery life percentage
-defaults write com.apple.menuextra.battery ShowPercent -string YES
-
 killall Dock
---
+++ osx/menu.defaults.sh
@@ -0,0 +1,9 @@
+# Sets reasonable OS X defaults.
+
+# Show battery life percentage
+defaults write com.apple.menuextra.battery ShowPercent -string YES
+
+# Setup system menubar icons
+defaults write com.apple.systemuiserver menuExtras -array "/System/Library/CoreServices/Menu Extras/Bluetooth.menu" "/System/Library/CoreServices/Menu Extras/Volume.menu" "/System/Library/CoreServices/Menu Extras/AirPort.menu" "/System/Library/CoreServices/Menu Extras/Battery.menu" "/System/Library/CoreServices/Menu Extras/Clock.menu"
+
+killall SystemUIServer
